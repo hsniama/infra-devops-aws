4e1e74ac1df1abd831dcaded78904f7d8260b9d1
Henry Niama <henryniama@hotmail.com>
2026-02-02 15:46:10 -0500

    Configuracion inicial de terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,62 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type = string
+  validation {
+    condition     = contains(["test", "prod"], var.environment)
+    error_message = "environment must be 'test' or 'prod'."
+  }
+}
+
+variable "project" {
+  type    = string
+  default = "devops-assessment"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "eks_name" {
+  type = string
+}
+
+variable "node_instance_types" {
+  type    = list(string)
+  default = ["t3.medium"]
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 5
+}
+
+variable "ecr_repo_name" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
